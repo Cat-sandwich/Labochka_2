3e1c2cabc2c19a8fac4933859e2cc60fc3beeaaa
Cat-sandwich <beni@ya.ru>
2022-10-16 16:41:54 +0400

    the end result
    the function of adding paths to a csv file has been added

--- csv_file.py
+++ csv_file.py
@@ -1,14 +1,19 @@
 import csv
 import os.path
 
-def add_to_csv (file_name, path):
+def add_to_csv (path_dataset, paths_txt):
     print("hey")
     with open('dataset.csv','w+', encoding='utf-8', newline='') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(["Absolute path", "Relative path", "Class"])
 
-        number = sum(os.path.isfile(os.path.join(path, f)) for f in os.listdir(path)) + 1
-        print(number)
+        for i in range (1, len(paths_txt)):
+            class_txt = str(paths_txt[i]).split('\\')
+            print(class_txt[1])
+            writer.writerow([f'{ (path_dataset + str(paths_txt[i])).replace(" ","")}', f'..\\dataset{(str(paths_txt[i])).replace(" ","")}', f'{class_txt[1]}'])
+            print(i)
+
+        
 
         
 def find_path_txt (path_dataset):
@@ -20,15 +25,10 @@ def find_path_txt (path_dataset):
 
         for j in range (0, count):
             path_txt = folder_name + f'\\{(j): 05}' + '.txt'
-            print(f'{folder_name}: {(j): 05}')
-            paths_txt.append(path_txt[len(path_dataset):])
+            #print(f'{folder_name}: {(j): 05}')
+            paths_txt.append(path_txt)
     
     return paths_txt
-
-
-
-
-
 
 
 
@@ -40,3 +40,8 @@ if __name__ == "__main__":
     print(path_dataset)
     
     paths_txt = find_path_txt(path_dataset)
+    print(paths_txt[1])
+
+    add_to_csv (path_dataset, paths_txt)
+    
+    print("Готово!")
